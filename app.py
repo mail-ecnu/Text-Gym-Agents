b3d041604f3aac3854b276acf804c372270d8776
hzxwonder <2354693781@qq.com>
2024-01-09 13:03:45 +0000

    github action test 3

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def main_progress(
 
 if __name__ == "__main__":
 
-    # Github action test 2
+    # Github action test 3
 
     # install Atari ROMs
     subprocess.run(['AutoROM', '--accept-license'])
