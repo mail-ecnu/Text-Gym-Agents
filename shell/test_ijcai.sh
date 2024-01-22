e533bfd52847ca835f7cf9b2ae1f9f9b5a8570ff
JarvisK <2667356534@qq.com>
2024-01-22 21:09:16 +0800

    adapt cot actor

--- shell/test_ijcai.sh
+++ shell/test_ijcai.sh
@@ -1,10 +1,10 @@
 # CoT 
 ## Atari
-python main_reflexion.py --env_name RepresentedPong-v0 --init_summarizer RepresentedPong_init_translator --curr_summarizer RepresentedPong_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
+python main_reflexion.py --env_name RepresentedPong-v0 --init_summarizer RepresentedPong_init_translator --curr_summarizer RepresentedPong_basic_translator --decider cot_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
 
-python main_reflexion.py --env_name RepresentedMsPacman-v0 --init_summarizer RepresentedMsPacman_init_translator --curr_summarizer RepresentedMsPacman_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
+python main_reflexion.py --env_name RepresentedMsPacman-v0 --init_summarizer RepresentedMsPacman_init_translator --curr_summarizer RepresentedMsPacman_basic_translator --decider cot_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
 
-python main_reflexion.py --env_name RepresentedMontezumaRevenge-v0 --init_summarizer RepresentedMontezumaRevenge_init_translator --curr_summarizer RepresentedMontezumaRevenge_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
+python main_reflexion.py --env_name RepresentedMontezumaRevenge-v0 --init_summarizer RepresentedMontezumaRevenge_init_translator --curr_summarizer RepresentedMontezumaRevenge_basic_translator --decider cot_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
 
 ## Mujoco
 python main_reflexion.py --env_name Hopper-v4 --init_summarizer hopper_init_translator --curr_summarizer hopper_basic_translator --decider cot_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
