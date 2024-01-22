469ef65acd8b86ec1e9ba458ea4ed72ad8788efd
JarvisK <2667356534@qq.com>
2024-01-22 16:41:48 +0800

    update conda env yaml

--- shell/test_ijcai.sh
+++ shell/test_ijcai.sh
@@ -1,6 +1,6 @@
 # CoT 
 ## Atari
-python main_reflexion.py --env_name RepresentedPong-v0 --init_summarizer RepresentedPong_init_translator --curr_summarizer RepresentedPong_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0
+python main_reflexion.py --env_name RepresentedPong-v0 --init_summarizer RepresentedPong_init_translator --curr_summarizer RepresentedPong_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0 --gpt_version gpt-3.5-turbo-1106
 
 python main_reflexion.py --env_name RepresentedMsPacman-v0 --init_summarizer RepresentedMsPacman_init_translator --curr_summarizer RepresentedMsPacman_basic_translator --decider naive_actor --prompt_level 1 --num_trails 1 --seed 0 
 
